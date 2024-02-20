2f06f13014099459a38dcd134e3e77586bc98653
Jacob Scott Brown <jacbro@comp431sp24b.cs.unc.edu>
2024-02-19 23:43:53 -0500

    Fix IO bugs

--- Client.py
+++ Client.py
@@ -2,11 +2,45 @@
 # Jacob Brown
 # I pledge the Comp 431 honor code.
 
-def main()
+from socket import *
+import sys
+
+def main():
     """
     Starting point of the SMTP client.
     """
+    try:
+        server_name: str = "comp431sp24.cs.unc.edu"
+        server_port: int = 9954
 
+        client_socket = socket(AF_INET, SOCK_STREAM)
+        client_socket.connect((server_name, server_port))
+
+        initial_msg: str = client_socket.recv(1024).decode()
+        print(initial_msg)
+
+        print("responding...")
+        initial_res: str = "HELO comp431sp24b.cs.unc.edu\n"
+        client_socket.send(initial_res.encode())
+
+        greet_msg: str = client_socket.recv(1024).decode()
+        print(greet_msg)
+
+        for line in sys.stdin:
+            command = line
+
+            client_socket.send(command.encode())
+
+            if (command == "QUIT\n"):
+                break
+
+            res: str = client_socket.recv(1024).decode()
+            print(res)
+
+        closing_msg: str = client_socket.recv(1024).decode()
+        print(closing_msg)
+    finally:
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
