7f6dd985fa789013d4b590bc6bcafee2efe2a86e
Jacob Scott Brown <jacbro@comp431sp24b.cs.unc.edu>
2024-02-19 20:00:00 -0500

    Create mock client main method

--- Client.py
+++ Client.py
@@ -1,3 +1,12 @@
 """SMTP Client that takes input from the user and transmits an SMTP message to a server via TCP."""
 # Jacob Brown
 # I pledge the Comp 431 honor code.
+
+def main()
+    """
+    Starting point of the SMTP client.
+    """
+
+
+if __name__ == "__main__":
+    main()
